4015fdb265acb435d0c9ae28223b4ae9e5d9bb2f
CQZ <752521865@qq.com>
2019-10-21 11:21:04 +0800

    IMoMei-SpringClou 提交啦！~

--- IMoMei-SpringCloud-Center/IMoMei-SpringCloud-Center-Config/src/main/java/com/imomei/CenterConfigApp.java
+++ IMoMei-SpringCloud-Center/IMoMei-SpringCloud-Center-Config/src/main/java/com/imomei/CenterConfigApp.java
@@ -3,9 +3,11 @@ package com.imomei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class CenterConfigApp {
     public static void main(String[] args) {
         SpringApplication.run(CenterConfigApp.class, args);
