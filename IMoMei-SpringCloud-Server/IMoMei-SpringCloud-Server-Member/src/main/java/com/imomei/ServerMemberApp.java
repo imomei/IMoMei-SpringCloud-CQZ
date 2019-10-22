1d5ae8571fb7df5923ad96fb520f403d7c3815f7
CQZ <752521865@qq.com>
2019-10-22 16:53:08 +0800

    步入正轨之提交

--- IMoMei-SpringCloud-Server/IMoMei-SpringCloud-Server-Member/src/main/java/com/imomei/ServerMemberApp.java
+++ IMoMei-SpringCloud-Server/IMoMei-SpringCloud-Server-Member/src/main/java/com/imomei/ServerMemberApp.java
@@ -1,5 +1,6 @@
 package com.imomei;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.imomei.dao")
 public class ServerMemberApp {
     public static void main(String[] args) {
         SpringApplication.run(ServerMemberApp.class, args);
